35c03d161c8719e4fcb5d4b3bf4a20775b8ec452
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-03-18 03:32:55 +0530

    data updates for recruiters

--- components/tables/user-tables/cell-action.tsx
+++ components/tables/user-tables/cell-action.tsx
@@ -100,14 +100,14 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
           <DropdownMenuItem onClick={() => setOpen(true)}>
             <Linkedin className="mr-2 h-4 w-4" /> Send LinkedIn Message
           </DropdownMenuItem>
-          <DropdownMenuItem
+          {/* <DropdownMenuItem
             onClick={() => router.push(`/dashboard/user/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" /> Update
           </DropdownMenuItem>
           <DropdownMenuItem onClick={() => setOpen(true)}>
             <Trash className="mr-2 h-4 w-4" /> Delete
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
         </DropdownMenuContent>
       </DropdownMenu>
     </>
