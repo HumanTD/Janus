af3f22996bf6c9b66f9bde861b9b9b88d99374cf
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-03-18 15:25:05 +0530

    edgestore file upload init

--- app/(dashboard)/dashboard/profile/page.tsx
+++ app/(dashboard)/dashboard/profile/page.tsx
@@ -4,16 +4,20 @@ import { CreateProfileOne } from "@/components/forms/user-profile-stepper/create
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Upload } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useRef } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useRef, useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { SingleImageDropzone } from "@/components/input/EdgeStoreInput";
+import { Button } from "@/components/ui/button";
 
 const breadcrumbItems = [{ title: "Profile", link: "/dashboard/profile" }];
 export default function page() {
   const { data: session } = useSession();
   if (!session) return null;
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const { edgestore } = useEdgeStore();
+  const [file, setFile] = useState<File>();
 
   console.log(session);
   return (
@@ -24,13 +28,15 @@ export default function page() {
           Hi, {session?.user?.name} 👋
         </h2>
         <h1>Upload your resume here!</h1>
-       <div className="flex cursor-pointer">
-       <Upload className="mx-2" onClick={()=>{inputRef.current?.click()}}></Upload>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          {/* <Label htmlFor="picture">Picture</Label> */}
-          <Input id="resume" type="file" className="cursor-pointer" ref={inputRef} />
-        </div>
-       </div>
+        <SingleImageDropzone
+          width={200}
+          height={200}
+          value={file}
+          onChange={(file) => {
+            setFile(file);
+          }}
+        />
+        <Button type="submit">Upload</Button>
         {/* <CreateProfileOne categories={[]} initialData={null} /> */}
       </div>
     </ScrollArea>
